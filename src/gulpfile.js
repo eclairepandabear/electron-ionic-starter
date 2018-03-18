9165377bad34ebb53169c4b88253eb1358fe330c
Hayden Braxton <hayden.braxton.dev@gmail.com>
2018-03-18 18:38:22 -0400

    initial commit. borrowed from Polyonic

---
+++ src/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp')
+const electron = require('electron-connect').server.create()
+const env = require('gulp-env')
+const config = require('@ionic/app-scripts/dist/util/config')
+const ionic = require('@ionic/app-scripts')
+
+gulp.task('electron-live', function () {
+  env.set({
+    NODE_ENV: 'development'
+  })
+  // Start browser process
+  electron.start()
+
+  gulp.watch('app.js', electron.restart)
+
+  // Reload renderer process
+  gulp.watch([
+    // 'app.js',
+    'www/js/app.js',
+    'www/**/*.html',
+    'www/**/*.css',
+    'www/**/*.js'], electron.reload)
+})
+
+gulp.task('dev', function () {
+  ionic.watch(config.generateContext())
+  .then(function () {
+    gulp.start('electron-live')
+  })
+  .catch(function (err) {
+    console.log('Error starting watch: ', err)
+  })
+})
